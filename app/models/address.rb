cc0c05e073f4ccf55b5bb314acb7d0861bdb398c
github username <github email address>
2019-07-05 09:40:42 -0600

    Done.

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,10 @@
 class Address < ActiveRecord::Base
   belongs_to :person
 end
+
+# class Song < ActiveRecord::Base
+#   def artist_attributes=(artist)
+#     self.artist = Artist.find_or_create_by(name: artist[:name])
+#     self.artist.update(artist)
+#   end
+# end
